06828fbe65f7d68e07ae409f3a2992d1e034733e
Phildo <Phildo211@gmail.com>
2013-01-06 21:20:30 +0000

    functioning! also, objects can be registered to multiple lists. readme updated to reflect functionality

--- registrationlist.js
+++ registrationlist.js
@@ -1,75 +1,98 @@
-var RegistrationList = function()
+var RegistrationList = function(identifier)
 {
   var self = this;
+  this.identifier = identifier;
 
-  function RegListNode(content)
+  var RNode = function(content)
   {
     var node = this;
     node.prev = null;
     node.next = null;
     node.content = content;
+
+    if(node.content !== null && typeof node.content.RNodeMap === 'undefined')
+    {
+      Object.defineProperty(node.content, "RNodeMap", {
+        enumerable:false,
+        configurable:true,
+        writable:true,
+        value:{}
+      });
+    }
   }
 
-  function insertNodeAfter(node, prevNode)
+  this.head = new RNode(null);
+  this.tail = new RNode(null);
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
+
+  var insertNodeAfter = function(node, prevNode)
   {
     node.prev = prevNode;
     node.next = prevNode.next;
     node.prev.next = node;
     node.next.prev = node;
 
-    if(if node.content !== null)
+    if(node.content !== null)
     {
-      Object.defineProperty(node.content, "registrationListNode", {
-        enumerable:false,
-        configurable:true,
-        writable:true,
-        value:node
-      });
+      node.content.RNodeMap[self.identifier] = node;
     }
 
     return node;
   }
 
-  function removeNode(node)
+  var removeNode = function(node)
   {
     node.prev.next = node.next;
     node.next.prev = node.prev;
-
     node.next = null;
     node.prev = null;
-
-    delete node.content.registrationListNode;
-
+  
+    if(node.content !== null)
+    {
+      delete node.content.RNodeMap[self.identifier];
+    }
+  
     return node;
   }
 
-  var head = new RegListNode(null);
-  var tail = new RegListNode(null);
-  head.next = tail;
-  tail.prev = head;
+  self.register = function(content)
+  {
+    insertNodeAfter(new RNode(content), self.head);
+  }
+  
+  self.unregister = function(content)
+  {
+    removeNode(content.RNodeMap[self.identifier]);
+  }
+  
+  self.moveMemberToList = function(content, list)
+  {
+    list.register(removeNode(content.RNodeMap[self.identifier]));
+  }
+  
+  self.performOnMembers = function(func)
+  {
+    var node = self.head;
+    while(node.next != null)
+    {
+      if(node.content !== null)
+        func(node.content);
+      node = node.next;
+    }
+  }
 }
-
-RegistrationList.prototype.register = function(content)
+  
+RegistrationList.prototype.toString = function()
 {
-  insertNodeAfter(new RegListNode(content), head);
-}
-
-RegistrationList.prototype.unregister = function(content)
-{
-  removeNode(content.registrationListNode);
-}
-
-RegistrationList.prototype.moveToList = function(content, list)
-{
-  list.register(removeNode(content.registrationListNode));
-}
-
-RegistrationList.prototype.performOnMembers = function(func)
-{
-  var node = head;
+  var str = "";
+  var node = this.head;
+  var i = 0;
   while(node.next != null)
   {
-    func(node.content);
     node = node.next;
+    if(node.content !== null)
+      str += node.content.toString()+",";
   }
+  return str;
 }
